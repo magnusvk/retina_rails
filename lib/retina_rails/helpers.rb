fe869c7da06034fd6dd7d2e3961d564cc2dc49d8
Magnus von Koeller <magnus@vonkoeller.de>
2013-04-22 13:22:26 -0400

    pull out helper for path to retina image

--- lib/retina_rails/helpers.rb
+++ lib/retina_rails/helpers.rb
@@ -6,17 +6,21 @@ module ActionView
         retina = options.delete(:retina)
 
         if retina
-          retina_source = source.split('.')
-          filename      = retina_source.slice!(-2)
-          retina_source = retina_source.insert(-2, "#{filename}@2x").join('.')
-
           options[:data] ||= {}
-          options[:data].merge!(:at2x => path_to_image(retina_source))
+          options[:data].merge!(:at2x => path_to_retina_image(source))
         end
 
         image_tag_without_retina(source, options)
       end
       alias_method_chain :image_tag, :retina
+
+      def path_to_retina_image(source)
+        retina_source = source.split('.')
+        filename      = retina_source.slice!(-2)
+        retina_source = retina_source.insert(-2, "#{filename}@2x").join('.')
+
+        path_to_image(retina_source)
+      end
 
     end
   end
